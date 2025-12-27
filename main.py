1b15097e83380782b8a96ab5d7acb46ff6a49fab
Sameep Kulkarni <kulsameep325@gmail.com>
2025-12-27 15:14:44 +0530

    integrated config.py

--- main.py
+++ main.py
@@ -10,29 +10,30 @@ data_dir = Path.cwd() / "parquet_files"
 from backtest.data import load_data, resample_ohlcv
 from backtest.signal import create_volume_moving_average,candlestick_colours,reversal,engulfing, volume_check, signal, entry
 from backtest.position_sizing import stop_loss, risk_in_points, take_profit, calc_quantity
-from backtest.execution import simulate_trade, post_processing, trades_df
+from backtest.execution import simulate_trade, post_processing, trades_df, result
 from backtest.costs import transaction_costs
+from backtest.config import rr, lookback_window, resample_timeframe, monetary_risk
 
 first_test = os.path.join(data_dir, "RELIANCE.parquet")
 reliance = load_data(first_test)
-reliance = resample_ohlcv(reliance, "5min")
-reliance = create_volume_moving_average(reliance, 8)
+reliance = resample_ohlcv(reliance, resample_timeframe)
+reliance = create_volume_moving_average(reliance, lookback_window)
 reliance = candlestick_colours(reliance)
 reliance = reversal(reliance) 
 reliance = engulfing(reliance)
-reliance = volume_check(reliance, candles = 8)
+reliance = volume_check(reliance, candles = lookback_window)
 reliance = signal(reliance)
 reliance = entry(reliance)
 reliance = stop_loss(reliance)
 reliance = risk_in_points(reliance)
-reliance = take_profit(reliance, rr = 2)
-reliance = calc_quantity(reliance, monetary_risk=1000)
+reliance = take_profit(reliance, rr = rr)
+reliance = calc_quantity(reliance, monetary_risk= monetary_risk)
 reliance = simulate_trade(reliance)
 reliance = transaction_costs(reliance)
 reliance = post_processing(reliance)
 reliance.to_csv(r'analysis\whole_df.csv')
 trades = trades_df(reliance)
 trades.to_csv(r'analysis\trades.csv')
-results = result(reliance)
-results = pd.DataFrame(results, index=False)
+results = result(reliance['net_pnl'])
+results = pd.DataFrame([results])
 results.to_csv(r'analysis\result.csv')
