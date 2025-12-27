f9deae9ceb47032307adc3e03daaf0e854bf925c
Sameep Kulkarni <kulsameep325@gmail.com>
2025-12-27 14:10:16 +0530

    trade simulation and post processing

--- backtest/execution.py
+++ backtest/execution.py
@@ -59,3 +59,44 @@ def simulate_trade(df):
 
     return df
 
+# post processing logic
+def post_processing(df): 
+  df = df.copy() 
+  completed_trade = df['exit_time'].notna()
+  df.loc[completed_trade,"pnl_points"] = (df.loc[completed_trade,"exit_price"] - df.loc[completed_trade,"entry_price"])* df.loc[completed_trade,"signal"] 
+  df.loc[completed_trade,"monetary_pnl"] = df.loc[completed_trade,"pnl_points"] * df.loc[completed_trade,"quantity"] 
+  df['net_pnl'] = df['monetary_pnl'] - df['total_costs']
+  df["equity"] = df["net_pnl"].fillna(0).cumsum() 
+  df["holding_time"] = df["exit_time"] - df["entry_time"]
+  df["trade_direction"] = np.where(df["signal"]==1,"buy",np.where(df["signal"]==-1,"sell",""))
+  df['drawdown'] = df['equity'].cummax() - df['equity']
+  return df
+
+
+# final results logic
+def result(pnl):
+  pnl = pnl.dropna()
+  pnl = pnl[pnl!=0]
+  wins = pnl[pnl>0]
+  losses = pnl[pnl<0]
+  avg_win = wins.mean()
+  avg_loss = losses.mean()
+  win_rate = len(wins)/len(pnl)
+  loss_rate = len(losses)/len(pnl)
+  expectancy = avg_win*win_rate+avg_loss*loss_rate
+  drawdown = max(pnl.fillna(0).cumsum().cummax() - pnl.fillna(0).cumsum()) 
+  return {
+    "avg_win": avg_win,
+    "avg_loss": avg_loss,
+    "win_rate": win_rate,
+    "loss_rate": loss_rate,
+    "expectancy": expectancy,
+    "drawdown": drawdown
+  } 
+  
+# trades dataframe logic
+def trades_df(df):
+  df =df.copy()
+  trades_df = df[['trade_direction','quantity','entry_time','exit_time','stop_loss','take_profit','risk_in_points','monetary_pnl','total_costs', 'net_pnl', 'holding_time','equity','drawdown']]
+  trades_df = trades_df[trades_df['exit_time'].notna()]
+  return trades_df
