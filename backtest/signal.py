fb4332bd8e790f4586dbfbf2dc094900699fd57b
Sameep Kulkarni <kulsameep325@gmail.com>
2025-12-27 14:10:47 +0530

    trade signal

--- backtest/signal.py
+++ backtest/signal.py
@@ -1,3 +1,18 @@
+# create volume moving average
+def create_volume_moving_average(df, candles = 8):
+  df = df.copy()
+  df[f"vma_{candles}"] = df["volume"].rolling(window=candles).mean()  
+  return df
+
+# candle colour logic
+def candlestick_colours(df):
+  df = df.copy()
+  df["candle"] = np.where(
+    df["open"]>df["close"],"red", 
+    np.where(df["open"]==df["close"],"white","green")
+  )
+  return df
+
 # reversal logic
 def reversal(df):
   df = df.copy()
@@ -52,29 +67,3 @@ def entry(df):
   df.loc[long | short, "entry_price"] = df["open"]
   return df
 
-# sl logic
-def stop_loss(df):
-  df = df.copy()
-  df["stop_loss"] = 0.0
-  long = df["signal"] == 1
-  short = df["signal"] == -1 
-  df.loc[long, "stop_loss"] = df["low"].shift(1).rolling(2).min()
-  df.loc[short, "stop_loss"] = df["high"].shift(1).rolling(2).max()
-  return df
-
-# risk in points logic 
-def risk_in_points(df):
-  df = df.copy()
-  df["risk_in_points"]= np.nan 
-  signal_present = (df["signal"] == 1) | (df["signal"] == -1)
-  df.loc[signal_present, "risk_in_points"] = (df["open"] - df["stop_loss"])*(df["signal"])
-  return df 
-#exit logic
-def take_profit(df,rr= 2):
-  df = df.copy()
-  df["take_profit"] = np.nan
-  long = df["signal"] == 1
-  short = df["signal"] == -1 
-  df.loc[long, "take_profit"] = df["risk_in_points"]*rr + df["open"]
-  df.loc[short, "take_profit"] = df["open"] - df["risk_in_points"]*rr
-  return df
